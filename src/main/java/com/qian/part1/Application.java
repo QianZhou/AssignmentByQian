4436c5bb25f6e1df3cd1b10247e55b733225c3e7
QIAN ZHOU <awinqian@gmail.com>
2018-05-02 18:37:52 -0400

    Add getAllUsers()

--- src/main/java/com/qian/part1/Application.java
+++ src/main/java/com/qian/part1/Application.java
@@ -2,12 +2,16 @@ package com.qian.part1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Application {
-
+	
+	@RequestMapping("/")
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 }
+
+
